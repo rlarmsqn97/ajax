5570cece7b2f887b7259f408e25b35f771e2fb68
rlarmsqn97 <srmsqn@kyungmin.ac.kr>
2021-05-30 21:05:39 +0900

    회원등록바로

---
+++ src/user/User.java
@@ -0,0 +1,41 @@
+package user;
+
+public class User {
+	private String userName;
+	private int userAge;
+	private String userGender;
+	private String userEmail;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public int getUserAge() {
+		return userAge;
+	}
+
+	public void setUserAge(int userAge) {
+		this.userAge = userAge;
+	}
+
+	public String getUserGender() {
+		return userGender;
+	}
+
+	public void setUserGender(String userGender) {
+		this.userGender = userGender;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+}
---
+++ src/user/UserDAO.java
@@ -0,0 +1,61 @@
+package user;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class UserDAO {
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	public UserDAO() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/ajax";
+			String dbID = "root";
+			String dbPassword = "1234";
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL,dbID,dbPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<User> search(String userName) {
+		String SQL = "select * from user where userName like ?";
+		ArrayList<User> userList = new ArrayList<User>();
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, "%" + userName + "%" );
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				User user = new User();
+				user.setUserName(rs.getString(1));
+				user.setUserAge(rs.getInt(2));
+				user.setUserGender(rs.getString(3));
+				user.setUserEmail(rs.getString(4));
+				userList.add(user);
+			}
+		} catch (Exception e) {
+			
+		}
+		return  userList;
+	}
+	
+	public int register(User user) {
+		String SQL = "insert into user values(?, ?, ?, ?)";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1,  user.getUserName());
+			pstmt.setInt(2,  user.getUserAge());
+			pstmt.setString(3,  user.getUserGender());
+			pstmt.setString(4,  user.getUserEmail());
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1; //데이터베이스 오류
+	}
+}
